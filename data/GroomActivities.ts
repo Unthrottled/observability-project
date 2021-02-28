e138b8b900cd28e02662f45bcefcf286b0d16c6f
DATBOI <alexsimons9999@gmail.com>
2021-02-28 15:53:29 -0600

    Working on grooming activity data

--- data/GroomActivities.ts
+++ data/GroomActivities.ts
@@ -1,76 +1,60 @@
-import { stream_file } from "./ReadFile.ts";
 import { GUID } from "./Constants.ts";
+import { stream_file } from "./ReadFile.ts";
 
-export interface ColorType {
-  hex: string;
-  opacity: number;
+export enum ActivityType {
+  ACTIVE = 'ACTIVE',
+  PASSIVE = 'PASSIVE',
+  NA = 'NA',
 }
 
-export interface TacticalActivity {
-  id: string;
+export enum ActivityStrategy {
+  GENERIC = 'GENERIC',
+}
+
+export enum ActivityTimedType {
+  NA = 'NA',
+  NONE = 'NONE',
+  TIMER = 'TIMER',
+  STOP_WATCH = 'STOP_WATCH',
+}
+
+export interface ActivityContent {
+  uuid: string;
   name: string;
-  rank: number;
-  antecedenceTime?: number;
-  iconCustomization: {
-    background: ColorType;
-    line: ColorType;
-  };
-  categories: string[];
-  hidden?: boolean;
-  removed?: boolean;
+  timedType: ActivityTimedType;
+  type: ActivityType;
+  paused?: boolean;
+  autoStart?: boolean;
+  veryFirstActivity?: boolean;
+  activityID?: string;
+  duration?: number;
+  workStartedWomboCombo?: number;
+}
+
+export interface Activity {
+  antecedenceTime: number;
   guid: string;
+  content: ActivityContent;
 }
 
-export interface MinTacticalActivity {
-  i: string; // id
-  n: string; // name
-  c: string; // 1st background 2nd line color separated by 和 (missing #)
-  ct: string; // activitiy's categories separated by 和 (minified see categories.json)
+export interface MinifiedActivity {
+  t: number; // Antecedence Time
+  i: number; // id
 }
 
-const activities: MinTacticalActivity[] = [];
+const activities: MinifiedActivity[] = [];
 
-await stream_file("./jsonDump/tacticalActivity.json", (line) => {
-  const activity: TacticalActivity = JSON.parse(line);
-  if (activity.guid == GUID && !(activity.hidden || activity.removed)) {
+const tacticalActivityMapping = {};
+
+var row = 0;
+await stream_file("./jsonDump/history.json", (line: any) => {
+  const activity: Activity = JSON.parse(line);
+  if (activity.guid == GUID && activity?.content?.type === ActivityType.ACTIVE) {
     activities.push({
-      i: activity.id,
-      n: activity.name,
-      c: `${activity.iconCustomization.background.hex.substr(1)
-      }和${activity.iconCustomization.background.hex.substr(1)}`,
-      ct: activity.categories.join("和"),
+      i: row++,
+      t: activity.antecedenceTime,
     });
   }
 });
 
-const minifiedCategories: { [key: string]: number } = Object.keys(
-  activities
-    .flatMap((activity) => activity.ct.split("和"))
-    .reduce(
-      (accum, cat) => ({
-        ...accum,
-        [cat]: cat,
-      }),
-      {}
-    )
-)
-  .map((cat, index) => [cat, index])
-  .reduce(
-    (accum, [cat, index]) => ({
-      ...accum,
-      [cat]: index,
-    }),
-    {}
-  );
-
-const minifiedActivities = activities.map((tacticalActivity, index) => ({
-  ...tacticalActivity,
-  i: index,
-  ct: tacticalActivity.ct
-    .split("和")
-    .map((ct) => minifiedCategories[ct])
-    .join("和"),
-}));
-
-Deno.writeTextFileSync('./minifiedJson/categories.json', JSON.stringify(minifiedCategories));
-Deno.writeTextFileSync('./minifiedJson/activities.json', JSON.stringify(minifiedActivities));
+console.log(activities.length);
---
+++ data/GroomTacticalActivities.ts
@@ -0,0 +1,76 @@
+import { stream_file } from "./ReadFile.ts";
+import { GUID } from "./Constants.ts";
+
+export interface ColorType {
+  hex: string;
+  opacity: number;
+}
+
+export interface TacticalActivity {
+  id: string;
+  name: string;
+  rank: number;
+  antecedenceTime?: number;
+  iconCustomization: {
+    background: ColorType;
+    line: ColorType;
+  };
+  categories: string[];
+  hidden?: boolean;
+  removed?: boolean;
+  guid: string;
+}
+
+export interface MinTacticalActivity {
+  i: string; // id
+  n: string; // name
+  c: string; // 1st background 2nd line color separated by 和 (missing #)
+  ct: string; // activitiy's categories separated by 和 (minified see categories.json)
+}
+
+const activities: MinTacticalActivity[] = [];
+
+await stream_file("./jsonDump/tacticalActivity.json", (line) => {
+  const activity: TacticalActivity = JSON.parse(line);
+  if (activity.guid == GUID && !(activity.hidden || activity.removed)) {
+    activities.push({
+      i: activity.id,
+      n: activity.name,
+      c: `${activity.iconCustomization.background.hex.substr(1)
+      }和${activity.iconCustomization.background.hex.substr(1)}`,
+      ct: activity.categories.join("和"),
+    });
+  }
+});
+
+const minifiedCategories: { [key: string]: number } = Object.keys(
+  activities
+    .flatMap((activity) => activity.ct.split("和"))
+    .reduce(
+      (accum, cat) => ({
+        ...accum,
+        [cat]: cat,
+      }),
+      {}
+    )
+)
+  .map((cat, index) => [cat, index])
+  .reduce(
+    (accum, [cat, index]) => ({
+      ...accum,
+      [cat]: index,
+    }),
+    {}
+  );
+
+const minifiedActivities = activities.map((tacticalActivity, index) => ({
+  ...tacticalActivity,
+  i: index,
+  ct: tacticalActivity.ct
+    .split("和")
+    .map((ct) => minifiedCategories[ct])
+    .join("和"),
+}));
+
+Deno.writeTextFileSync('./minifiedJson/categories.json', JSON.stringify(minifiedCategories));
+Deno.writeTextFileSync('./minifiedJson/activities.json', JSON.stringify(minifiedActivities));
